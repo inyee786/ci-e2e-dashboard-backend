18e2ee7a3d85eac2ed9f5d7774db129df9b95a33
inyee786 <intakhab.ali@mayadata.io>
2019-02-26 20:56:31 +0530

    remove aws gcp and packet

--- src/build.js
+++ src/build.js
@@ -3,42 +3,42 @@ var gitlab_private_token = process.env.token;
 
 
 module.exports = {
-    cstor_pipeline:function() {
-        var cstor = {  
-            url: 'https://gitlab.openebs100.io/api/v4/projects/26/pipelines?ref=master',
-            headers: {'PRIVATE-TOKEN': gitlab_private_token}
-        };
-        return new Promise(function(resolve, reject){
-            request(cstor, function(err, response, body) {
-                if (err  || response.statusCode != 200) {
-                    reject(err);
-                } else {
-                    body = JSON.parse(body);
-                    resolve(body);
-                }
-            });
-        });
-    },
-    cstor_jobs:function(id) {
-        var cstor_job = {  
-            url: "https://gitlab.openebs100.io/api/v4/projects/26/pipelines/+"+id+"/jobs?per_page=50",
-            headers: {'PRIVATE-TOKEN': gitlab_private_token}
-        };
-        return new Promise(function(resolve, reject){
-            request(cstor_job, function(err, response, body) {
-                if (err  || response.statusCode != 200) {
-                    reject(err);
-                } else {
-                    body = JSON.parse(body);
-                    resolve(body);
-                }
-            });
-        });
-    },
+    // cstor_pipeline:function() {
+    //     var cstor = {  
+    //         url: 'https://gitlab.openebs100.io/api/v4/projects/26/pipelines?ref=master',
+    //         headers: {'PRIVATE-TOKEN': gitlab_private_token}
+    //     };
+    //     return new Promise(function(resolve, reject){
+    //         request(cstor, function(err, response, body) {
+    //             if (err  || response.statusCode != 200) {
+    //                 reject(err);
+    //             } else {
+    //                 body = JSON.parse(body);
+    //                 resolve(body);
+    //             }
+    //         });
+    //     });
+    // },
+    // cstor_jobs:function(id) {
+    //     var cstor_job = {  
+    //         url: "https://gitlab.openebs100.io/api/v4/projects/26/pipelines/+"+id+"/jobs?per_page=50",
+    //         headers: {'PRIVATE-TOKEN': gitlab_private_token}
+    //     };
+    //     return new Promise(function(resolve, reject){
+    //         request(cstor_job, function(err, response, body) {
+    //             if (err  || response.statusCode != 200) {
+    //                 reject(err);
+    //             } else {
+    //                 body = JSON.parse(body);
+    //                 resolve(body);
+    //             }
+    //         });
+    //     });
+    // },
 
     maya_pipeline:function() {
         var maya = {
-            url: "https://gitlab.openebs100.io/api/v4/projects/31/pipelines?ref=master",
+            url: "https://gitlab.openebs100.io/api/v4/projects/7/pipelines?ref=master",
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
@@ -57,7 +57,7 @@ module.exports = {
 
     maya_jobs:function(id) {
         var maya_job = {
-            url: "https://gitlab.openebs100.io/api/v4/projects/31/pipelines/+"+id+"/jobs?per_page=50",
+            url: "https://gitlab.openebs100.io/api/v4/projects/7/pipelines/+"+id+"/jobs?per_page=50",
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
@@ -76,7 +76,7 @@ module.exports = {
 
     jiva_pipeline:function() {
         var jiva = {
-            url: "https://gitlab.openebs100.io/api/v4/projects/28/pipelines?ref=master",
+            url: "https://gitlab.openebs100.io/api/v4/projects/6/pipelines?ref=master",
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
@@ -94,7 +94,7 @@ module.exports = {
     },
     jiva_jobs:function(id) {
         var jiva = {
-            url: "https://gitlab.openebs100.io/api/v4/projects/28/pipelines/+"+id+"/jobs?per_page=50",
+            url: "https://gitlab.openebs100.io/api/v4/projects/6/pipelines/+"+id+"/jobs?per_page=50",
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
